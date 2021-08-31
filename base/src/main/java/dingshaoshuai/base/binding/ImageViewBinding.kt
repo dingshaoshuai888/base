08fc4c5799b88598b3927fb760850d9dfac16f02
dingshaoshuai <xdingshaoshuai@163.com>
2021-08-31 14:43:02 +0800

    ImageLoader 添加 defaultDrawable

--- base/src/main/java/dingshaoshuai/base/binding/ImageViewBinding.kt
+++ base/src/main/java/dingshaoshuai/base/binding/ImageViewBinding.kt
@@ -1,5 +1,6 @@
 package dingshaoshuai.base.binding
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
@@ -12,4 +13,9 @@ import dingshaoshuai.base.feature.image.ImageLoaderProxy
 @BindingAdapter("binding:url", "binding:defaultResourceId", requireAll = true)
 fun bindingImage(imageView: ImageView, url: String, @DrawableRes defaultResourceId: Int) {
     ImageLoaderProxy.instance.load(imageView, url, defaultResourceId)
+}
+
+@BindingAdapter("binding:url", "binding:defaultDrawable", requireAll = true)
+fun bindingImage(imageView: ImageView, url: String, drawable: Drawable) {
+    ImageLoaderProxy.instance.load(imageView, url, drawable)
 }
--- base/src/main/java/dingshaoshuai/base/feature/image/ImageLoader.kt
+++ base/src/main/java/dingshaoshuai/base/feature/image/ImageLoader.kt
@@ -1,6 +1,7 @@
 package dingshaoshuai.base.feature.image
 
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
@@ -12,6 +13,7 @@ import androidx.annotation.DrawableRes
 interface ImageLoader {
     fun load(imageView: ImageView, @DrawableRes resourceId: Int)
     fun load(imageView: ImageView, url: String, @DrawableRes defaultResourceId: Int)
-    fun load(imageView: ImageView, bitmap: Bitmap)
-    fun load(imageView: ImageView, uri: Uri)
+    fun load(imageView: ImageView, url: String, defaultDrawable: Drawable)
+    fun load(imageView: ImageView, bitmap: Bitmap, defaultDrawable: Drawable)
+    fun load(imageView: ImageView, uri: Uri, defaultDrawable: Drawable)
 }
